0083a89fd522bb714ea56ff0c24f96992b6f66fd
Yishai Biton <yishai946@gmail.com>
2026-02-13 02:06:16 +0200

    feat: add product crud

---
+++ server/prisma/migrations/20260212094425_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
